706a97a586419e7eb5a29297109c6e899e1cb4f5
Sava Virtosu <savionok@gmail.com>
2014-07-16 16:14:14 +0300

    new mode papaya :tangerine:

---
+++ core/mods/papaya/papaya.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+from core.ocr import CaptchonkaOCR
+
+from PIL import Image, ImageEnhance
+import cv2, cv2.cv as cv, numpy
+import helpers as ImageHelpers
+
+class CaptchonkaOCRMod(CaptchonkaOCR):
+  def checkCharacterSizes(self, width=0, height=0):
+    return (width > 5 or width * height >= 80) and height > 3 and width < 28 and height < 28
+
+  def cleanImage(self, processed):
+    self.newStep()
+    options = self.options
+
+    if options.verbose:
+      processed.save(options.output_dir + '/{}-processed.png'.format(self.getStep()))
+    processed = processed.convert("LA")
+    processed = processed.convert("P")
+    processed = self.cleanDenoise(processed)
+    processed = self.erodeAndDilate(processed)
+    processed = self.blackAndWhite(processed, 170)
+
+    return processed
+
+  def cleanDenoise(self, processed):
+    self.newStep()
+    options = self.options
+
+    clean = cv2.fastNlMeansDenoising(numpy.array(processed), None, 35, 4, 7)
+
+    processed = Image.fromarray(numpy.uint8(clean))
+
+    if options.verbose:
+      processed.save(options.output_dir + '/{}-denoise.png'.format(self.getStep()))
+
+    return processed
+
+  def erodeAndDilate(self, processed):
+    self.newStep()
+    options = self.options
+
+    contr = ImageEnhance.Contrast(processed)
+    processed = contr.enhance(1.1)
+
+    sharp = ImageEnhance.Sharpness(processed)
+    processed = sharp.enhance(1.9)
+
+    clean = cv2.fastNlMeansDenoising(numpy.array(processed), None, 10, 3, 7)
+    processed = Image.fromarray(numpy.uint8(clean))
+
+    if options.verbose:
+      processed.save(options.output_dir + '/{}-erode-and-dilate.png'.format(self.getStep()))
+
+    return processed
